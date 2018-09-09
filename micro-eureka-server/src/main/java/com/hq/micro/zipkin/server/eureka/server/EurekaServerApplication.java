57ac053b53f088be7dbc9a71ad4118226f8d638b
YuanCoder <1073625256@qq.com>
2018-09-09 10:15:20 +0800

    修改

--- micro-eureka-server/src/main/java/com/hq/micro/zipkin/server/eureka/server/EurekaServerApplication.java
+++ micro-eureka-server/src/main/java/com/hq/micro/zipkin/server/eureka/server/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.hq.micro.zipkin.server.eureka.server;/**
  * Created by Yuanjp on 2017/12/6 0006.
  */
 
+import com.netflix.appinfo.InstanceInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -24,6 +26,20 @@ public class EurekaServerApplication {
      * @param args
      */
     public static void main(String[] args){
+/*
+      Eureka核心类：
+          InstanceInfo 代表注册服务实例
+          LeaseInfo 标识应用实例的租约信息 这些参数主要用于标识应用实例的心跳情况，如：约定的心跳周期、租约有效期、最近一次续约时间
+          ServiceInstance 是对服务发现实例信息的抽象接口，约定了服务发现的实例应用有哪些通用的信息
+          InstanceInfo.InstanceStatus  是一个枚举用于标识服务实例的状态
+      服务的核心操作
+          服务注册（register）
+          服务下线（cancel）
+          服务租约（renew）
+          服务剔除（evict）
+*/
+
+
 
         SpringApplication.run(EurekaServerApplication.class, args);
         logger.info("====================启动成功!====================");

f5cd7688637ee52c3d308f8e17c6674f973435f6
yuanjiangpeng <1073625256@qq.com>
2018-09-19 12:02:43 +0800

    eureka 参数补充

--- micro-eureka-server/src/main/java/com/hq/micro/eureka/server/EurekaServerApplication.java
+++ micro-eureka-server/src/main/java/com/hq/micro/eureka/server/EurekaServerApplication.java
@@ -1,14 +1,11 @@
-package com.hq.micro.zipkin.server.eureka.server;/**
+package com.hq.micro.eureka.server;/**
  * Created by Yuanjp on 2017/12/6 0006.
  */
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.eureka.lease.LeaseManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -44,8 +41,8 @@ public class EurekaServerApplication {
         在分布式系统设计里头，通常需要对应用实例的存活进行键康检查，这里比较关键的问题就是要处理好网络偶尔抖动或短哲不可用时造成的误判。
         另外Eureka Server端与Client端之间如果出现网络分区问题，在极端I青况下可能会使得Eureka Server清空部分服务的 实例列表，这个将严重影响
         到Eureka Server的Availibility属性。因此Eureka Server引入了SELF PRESERVATION机制。
-        Eureka Client端与Server端之间有个租约，Client要定时发送心跳来维持这个租约，表示自己还存活看。
-        Eureka通过当前注册的实例数，去计算每分钟应该从应用实例接收到的心跳数，如果最近一分钟接收到的续约的次数小于指定阀值的话，则关闭租约失效剔除， 禁止定时任务剔除失效的实例，从而保护注册信息。
+        Eureka Client端与Server端之间有个租约，Client要定时(默认30秒)发送心跳来维持这个租约，表示自己还存活看。
+        Eureka通过当前注册的实例数，去计算每分钟应该从应用实例接收到的心跳数，如果最近一分钟接收到的续约的次数小于指定阀值（比率默认0.85）的话，则关闭租约失效剔除， 禁止定时任务剔除失效的实例，从而保护注册信息。
 */
         SpringApplication.run(EurekaServerApplication.class, args);
         logger.info("====================启动成功!====================");
